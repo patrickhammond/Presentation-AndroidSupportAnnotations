29e525d98b2aba01f7e4b03b3dbb74e3bc21e421
Patrick Hammond <patrick@madebyatomicrobot.com>
2015-08-18 17:06:08 -0400

    Creating some slightly better code.

--- app/src/main/java/com/demo/buggyapp/MainActivity.java
+++ app/src/main/java/com/demo/buggyapp/MainActivity.java
@@ -1,10 +1,16 @@
 package com.demo.buggyapp;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.CheckResult;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.annotation.RequiresPermission;
 import android.support.annotation.VisibleForTesting;
 import android.util.Log;
+import android.widget.Toast;
 
 import java.io.BufferedInputStream;
 import java.io.File;
@@ -28,7 +34,7 @@ public class MainActivity extends BaseActivity {
         return new Intent(context, MainActivity.class);
     }
 
-    private String username;
+    @Nullable private String username;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,31 +44,37 @@ public class MainActivity extends BaseActivity {
         username = getIntent().getStringExtra("username");
 
         // Bug #2 - Passing in a color ref vs color value
-        getWindow().getDecorView().setBackgroundColor(android.R.color.black);
+        getWindow().getDecorView().setBackgroundColor(getResources().getColor(android.R.color.black));
     }
 
     @Override
     protected void doSomeSetup() {
         // Bug #4a - Not calling base class implementation like the documentation states
 
-        setTitle(username);  // Bug #1 - Username is optional, might be null.
+        String message = username == null ? "No username" : String.format("The username is %d long", calculateUsernameLength(username));
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
 
         try {
             // Bug #3 - app does not have network permissions in the manifest
             checkinWithHome();
 
             // Bug #4b - app doesn't handle when a file is not deleted
-            deleteOldFile();
+            boolean deleted = deleteOldFile();
+            if (!deleted) {
+                Toast.makeText(this, "Doh! File not deleted", Toast.LENGTH_SHORT).show();
+            }
         } catch (Exception ex) {
             Log.e("DEBUG", "don't do this in production", ex);
         }
     }
 
+    @CheckResult
     private boolean deleteOldFile() throws Exception {
         File file = new File("myfile");
         return file.delete();
     }
 
+    @RequiresPermission(Manifest.permission.INTERNET)
     public void checkinWithHome() throws IOException {
         // Copied from http://developer.android.com/reference/java/net/HttpURLConnection.html
         URL url = new URL("http://www.home.com/checkin");
@@ -76,7 +88,8 @@ public class MainActivity extends BaseActivity {
     }
 
     // Bug #5 - not private, visible for testing, not sure why it isn't private
-    int calculateUsernameLength(String username) {
+    @VisibleForTesting
+    int calculateUsernameLength(@NonNull String username) {
         return username.length();
     }
 }
